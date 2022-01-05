5e3264b2b4e82bec95584e38ce57312ba37623e2
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-05 12:16:52 +0100

    Fix format in createnewrec and sidebarmenu

--- src/components/createnewrec.tsx
+++ src/components/createnewrec.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function CreateNewRec(): JSX.Element {
-    const [recommend, setRecommend] = useState<string>()
+  const [recommend, setRecommend] = useState<string>();
 
   return (
     <>
@@ -9,9 +9,13 @@ export default function CreateNewRec(): JSX.Element {
       <input placeholder="Title"></input>
       <input placeholder="Link"></input>
       <input placeholder="Author"></input>
-      <button onClick={()=> setRecommend("Recommended")}>Recommended</button>
-      <button onClick={()=> setRecommend("Not Recommended")}>Not Recommended</button>
-      <button onClick={()=> setRecommend("Looks Interesting")}>Looks Interesting</button>
+      <button onClick={() => setRecommend("Recommended")}>Recommended</button>
+      <button onClick={() => setRecommend("Not Recommended")}>
+        Not Recommended
+      </button>
+      <button onClick={() => setRecommend("Looks Interesting")}>
+        Looks Interesting
+      </button>
       <textarea placeholder="Explain why you would/wouldn't recommend or why it looks interesting"></textarea>
       <textarea placeholder="Description/Summary"></textarea>
       <button>Submit</button>
--- src/components/sidebarmenu.tsx
+++ src/components/sidebarmenu.tsx
@@ -1,17 +1,16 @@
-import CreateNewRec from './createnewrec';
+import CreateNewRec from "./createnewrec";
 
 export default function SideBarMenu(): JSX.Element {
-    
   return (
     <>
-        <button>Create New Recommendation</button>
-        <br/>
-        <button>Podcast</button>
-        <button>Articles</button>
-        <button>Exercises</button>
-        <button>Videos</button>
-        <br/>
-        <button>Study List</button>
+      <button>Create New Recommendation</button>
+      <br />
+      <button>Podcast</button>
+      <button>Articles</button>
+      <button>Exercises</button>
+      <button>Videos</button>
+      <br />
+      <button>Study List</button>
     </>
   );
 }
