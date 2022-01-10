c47323b316c6a56cfdd34a9fecf9faef20b01412
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-10 16:13:37 +0100

    Add tests for separateCapitalise function

--- src/utils/separateCapitalise.test.ts
+++ src/utils/separateCapitalise.test.ts
@@ -1,5 +1,9 @@
 import separateCapitalise from "./separateCapitalise";
 
 test("separateCapitalise returns the input string with the first letter capitalised", () => {
-    expect(separateCapitalise("hello world")).toStrictEqual("Hello world")
+    expect(separateCapitalise("hello world")).toStrictEqual("Hello World");
+    expect(separateCapitalise("hello-world")).toStrictEqual("Hello World");
+    expect(separateCapitalise("-world")).toStrictEqual("World");
+    expect(separateCapitalise(" hello world")).toStrictEqual("Hello World");
+    expect(separateCapitalise(" -world")).toStrictEqual("World");
 })
