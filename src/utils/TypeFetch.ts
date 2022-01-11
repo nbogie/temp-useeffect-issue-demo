841ca94a67c4585dff6376d520e1b827c9237454
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-11 14:48:43 +0100

    Add void type to typefetch

--- src/utils/TypeFetch.ts
+++ src/utils/TypeFetch.ts
@@ -1,9 +1,9 @@
 export default async function TypeFetch(): Promise<string[] | void> {
   try {
-    const APIres = await fetch("https://backend-c3c4.herokuapp.com/types");
-    const types = await APIres.json();
-    return types.data;
+    const APIres = await fetch('https://backend-c3c4.herokuapp.com/types')
+    const types = await APIres.json()
+    return types.data
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
