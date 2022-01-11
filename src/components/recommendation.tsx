6388529f52ffac230b19ccb524a912aa00449828
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-01-11 15:11:37 +0100

    Return jsonBody

--- src/components/recommendation.tsx
+++ src/components/recommendation.tsx
@@ -7,8 +7,8 @@ export default function Recommendation(props: recommendation): JSX.Element {
     const response = await fetch(
       `https://backend-c3c4.herokuapp.com/rec/${props.id}`
     );
-    // const jsonBody:httpResponseProps = await response.json();
-    return response;
+    const jsonBody = await response.json();
+    return jsonBody;
   };
   console.log(resource);
   return <div className="rec-preview"></div>;
