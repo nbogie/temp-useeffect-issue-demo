907e13b59d6845a1c5f323ecb46841b24c4a18a3
Jenna <jr13114@my.bristol.ac.uk>
2022-01-12 13:30:00 +0100

    tried fetching using currentRec, but is always set to 0 - needs work

--- src/components/recommendation.tsx
+++ src/components/recommendation.tsx
@@ -2,44 +2,51 @@ import "../css/recPreview.css";
 import { useEffect, useState } from "react";
 import { RecProps } from "../utils/RecProps";
 
-export default function Recommendation(): JSX.Element {
+interface CurrentRecProps {
+  currentRec: number;
+}
+
+export default function Recommendation({
+  currentRec,
+}: CurrentRecProps): JSX.Element {
   const [rec, setRec] = useState<RecProps>({
     recInfo: [],
     comments: [],
     tags: [],
   });
 
-  const fetchRec = async (id: number) => {
+  const fetchRec = async () => {
     const response = await fetch(
-      `https://backend-c3c4.herokuapp.com/rec/${id}`
+      `https://backend-c3c4.herokuapp.com/rec/${currentRec}`
     );
     const jsonBody = await response.json();
     setRec(jsonBody.data);
   };
 
   useEffect(() => {
-    fetchRec(1);
+    fetchRec();
   }, []);
-
+  console.log(currentRec);
   return (
-    <div>
-      {rec.recInfo.length !== 0 && (
-        <div>
-          <h1>{rec.recInfo[0].title}</h1>
-          <h3>By: {rec.recInfo[0].author}</h3>
-          <h4>
-            Submitted by: {rec.recInfo[0].name} on {rec.recInfo[0].submit_time}
-          </h4>
-          <p>
-            {rec.recInfo[0].status}: {rec.recInfo[0].reason}
-          </p>
-          <p>Summary: {rec.recInfo[0].summary}</p>
-          <h5>Comments:</h5>
-          <p>{rec.comments[0].comment}</p>
-          <p>{rec.comments[0].submit_time}</p>
-          <p>{rec.tags[0].tag}</p>
-        </div>
-      )}
-    </div>
+    <p>hello</p>
+    // <div>
+    //   {rec.recInfo.length !== 0 && (
+    //     <div>
+    //       <h1>{rec.recInfo[0].title}</h1>
+    //       <h3>By: {rec.recInfo[0].author}</h3>
+    //       <h4>
+    //         Submitted by: {rec.recInfo[0].name} on {rec.recInfo[0].submit_time}
+    //       </h4>
+    //       <p>
+    //         {rec.recInfo[0].status}: {rec.recInfo[0].reason}
+    //       </p>
+    //       <p>Summary: {rec.recInfo[0].summary}</p>
+    //       <h5>Comments:</h5>
+    //       <p>{rec.comments[0].comment}</p>
+    //       <p>{rec.comments[0].submit_time}</p>
+    //       <p>{rec.tags[0].tag}</p>
+    //     </div>
+    //   )}
+    // </div>
   );
 }
